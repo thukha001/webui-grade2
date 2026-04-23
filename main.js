6871d6ffa1e2e345109cfd4fb1e42b164afa2cff
thukha001 <thukha.shan2023@gamil.com>
2026-04-23 16:59:37 +0900

    my first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// const name = prompt("名前は？");
+// alert("こんにちは " + name + " さん！");
+
+const target = document.getElementById("target");
+target.textContent = "こんにちは!"
+target.style.color = "red";
+target.style.fontSize = "40px";
+target.style.backgroundColor = "yellow"
